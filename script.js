5f36749a2abf3c7aa0f83e2bdc04c2f843bab353
PrebenS <prebens.kodehode@gmail.com>
2023-06-09 13:55:39 +0200

    js comments

--- script.js
+++ script.js
@@ -6,18 +6,24 @@ buttons.forEach((button) => {
 });
 
 function buttonClick(event) {
+  // stores the css property from the clicked button
   const clickedProperty = event.target.dataset.property;
 
   buttons.forEach((button) => {
+    // checks if the property of the button matches the property of the clicked button
     if (button.dataset.property === clickedProperty) {
+      // removes the ".active" class from matching buttons
       button.classList.remove("active");
     }
   });
 
+  // adds ".active" class to clicked button
   event.target.classList.add("active");
 
+  // stores the css property and its value from html dataset of the clicked button
   const property = event.target.dataset.property;
   const value = event.target.dataset.value;
 
+  // changes the value of the corresponding css property
   flexboxEl.style[property] = value;
 }
